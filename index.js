e4bd71fdabd38207fd4881e4577856f50987e08c
Rakib665 <islammdr44@gmail.com>
2022-05-08 16:47:28 +0600

    after use middleware

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 5000
 require('dotenv').config()
+
+
+// middleware
+
+app.use(cors())
+app.use(express.json())
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boeyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+try{
+  await client.connect()
+const inventoryCollection = client.db('warehouse').collection('items')
+console.log('mg connect')
+app.get('/inventory', async(req,res)=>{
+  const query = {}
+  const cursor = inventoryCollection.find(query)
+  const items = await cursor.toArray()
+  res.send(items)
+})
+
+}
+finally{}
+
+
+}
+run().catch(console.dir)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
