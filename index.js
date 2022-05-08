1845a239747aafc53e65e73e21d127b7787e7124
Rakib665 <islammdr44@gmail.com>
2022-05-09 02:28:53 +0600

    after create update section

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.boeyp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,6 +24,13 @@ app.get('/inventory', async(req,res)=>{
   const cursor = inventoryCollection.find(query)
   const items = await cursor.toArray()
   res.send(items)
+})
+
+app.get('/inventory/:id', async(req,res)=>{
+  const id = req.params.id;
+  const query = {_id: ObjectId(id)}
+  const item = await inventoryCollection.findOne(query)
+  res.send(item)
 })
 
 }
